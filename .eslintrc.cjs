148cf4c0b9490ba567da985b31c6e19b7a869acb
quntaoz <quntaozheng@gmail.com>
2024-04-10 00:03:54 -0400

    fixed eslint for frontend

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,26 +1,17 @@
 module.exports = {
-  env: {
-    browser: true,
-    commonjs: true,
-    es2021: true,
-  },
-  extends: 'airbnb-base',
-  overrides: [
-    {
-      env: {
-        node: true,
-      },
-      files: [
-        '.eslintrc.{js,cjs}',
-      ],
-      parserOptions: {
-        sourceType: 'script',
-      },
-    },
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
   ],
-  parserOptions: {
-    ecmaVersion: 'latest',
-  },
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  settings: { react: { version: "18.2" } },
+  plugins: ["react-refresh"],
   rules: {
+    "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
   },
 };
