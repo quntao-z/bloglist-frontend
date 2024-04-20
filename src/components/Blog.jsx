c51bf8fac4562b900304afcf3d68d5b6b6179e6b
quntaoz <quntaozheng@gmail.com>
2024-04-19 23:48:40 -0400

    trying to add like button

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import blogService from '../services/blogs'
 
 const Blog = ({ blog }) => {
   const blogStyle = {
@@ -18,18 +19,33 @@ const Blog = ({ blog }) => {
   const viewButtonLabel = () => {
     return view ? "Hide" : "View"
   }
-  
+
+  const likeButton = () => {
+    const newBlogObject = {
+      title: blog.title,
+      author: blog.author,
+      url: blog.url,
+      likes: blog.likes + 1
+    }
+
+    const response = blogService.update(blog.id, newBlogObject)
+    console.log(response)
+  }
+
   return (
     <div style={blogStyle}>
       <div className='button-container'>
         <div>{blog.title} {blog.author}</div>
         <button onClick={handleView}>{viewButtonLabel()}</button>
-      </div>  
+      </div>
 
       { view ?
         <div>
           <div>{blog.url}</div>
-          <div>Likes: {blog.likes}</div>
+          <div>
+            Likes: {blog.likes}
+            <button onClick={likeButton}>like</button>
+          </div>
           <div>{blog.author}</div>
         </div>
         : null
--- src/services/blogs.js
+++ src/services/blogs.js
@@ -1,30 +1,37 @@
-import axios from 'axios';
+import axios from 'axios'
 
-const baseUrl = '/api/blogs';
+const baseUrl = '/api/blogs'
 
-let token = null;
+let token = null
 
 const setToken = (newToken) => {
-  token = `Bearer ${newToken}`;
-};
-
-const getAll = async () => {
-  const response = await axios.get(baseUrl, {
-    headers: {
-      authorization: token,
-    },
-  });
-  return response.data;
-};
-
-const create = async (blogObject) => {
-  const response = await axios.post(baseUrl, blogObject,
-  {
-    headers: {
-      authorization: token,
-    },
-  })
-  return response.data;
+        token = `Bearer ${newToken}`
 }
 
-export default { setToken, getAll, create };
+// Axios request interceptor to add token to all requests
+axios.interceptors.request.use(config => {
+        config.headers.authorization = token
+        return config
+}, error => {
+        return Promise.reject(error)
+})
+
+const getAll = async () => {
+        const response = await axios.get(baseUrl)
+        console.log(token)
+        return response.data
+}
+
+const create = async (blogObject) => {
+        const response = await axios.post(baseUrl, blogObject)
+        return response.data
+}
+
+const update = async (blogId, blogObject) => {
+        const response = await axios.put(baseUrl + `/${blogId}`, blogObject)
+
+        console.log(response)
+        return response.data
+}
+
+export default { setToken, getAll, create, update }
