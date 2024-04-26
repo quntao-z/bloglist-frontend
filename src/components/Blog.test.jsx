7c318d598d19621beaa02d480ac868eafac68745
quntaoz <quntaozheng@gmail.com>
2024-04-25 23:55:15 -0400

    try to test blog like button

--- src/components/Blog.test.jsx
+++ src/components/Blog.test.jsx
@@ -1,6 +1,9 @@
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
+import axios from 'axios'
+import MockAdapter from 'axios-mock-adapter'
+import { expect } from 'vitest'
 
 
 test('renders blog', () => {
@@ -45,8 +48,6 @@ test('view button click', async() => {
   const button = screen.getByText('View')
   await user.click(button)
 
-  screen.debug()
-
   const urlDiv = container.querySelector('.blog-url')
 
   expect(urlDiv).toHaveTextContent(
@@ -58,4 +59,32 @@ test('view button click', async() => {
   expect(likeDiv).toHaveTextContent(
     7
   )
+})
+
+test('blog like button', async () => {
+  const blog = {
+    _id: '5a422a851b54a676234d17f7',
+    title: 'React patterns',
+    author: 'Michael Chan',
+    url: 'https://reactpatterns.com/',
+    likes: 7,
+    __v: 0,
+    user: {
+      username: 'ghopper'
+    }
+  }
+
+  const mock = new MockAdapter(axios)
+  mock.onPut(`/api/blogs/${blog._id}`).reply(200, { ...blog, likes: blog.likes + 1 })
+
+  render(<Blog blog={blog} />)
+  const user = userEvent.setup()
+
+  const viewButton = screen.getByText('View')
+  await user.click(viewButton)
+
+  const likeButton = screen.getByText('like')
+  await user.click(likeButton)
+
+  expect(mock.history.put.length).toBe(1)
 })
