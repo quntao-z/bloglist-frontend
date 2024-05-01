36548f3fcad75b8eb0e210efb24884894126c6e8
quntaoz <quntaozheng@gmail.com>
2024-04-30 23:34:25 -0400

    id for login form

--- src/App.jsx
+++ src/App.jsx
@@ -78,6 +78,7 @@ const App = () => {
       <div>
           username
         <input
+          data-testid='username'
           type='text'
           value={username}
           name="Username"
@@ -87,6 +88,7 @@ const App = () => {
       <div>
           password
         <input
+          data-testid='password'
           type='password'
           value={password}
           name="Password"
