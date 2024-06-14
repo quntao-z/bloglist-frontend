dc5a8f5795c66cf8fe1c07f5ddf3b6aa71c6efe7
quntaoz <quntaozheng@gmail.com>
2024-06-13 23:56:17 -0400

    added blogreducer

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,21 @@
 import ReactDOM from "react-dom/client";
+import { configureStore } from '@reduxjs/toolkit'
+import { Provider } from 'react-redux'
 import App from "./App";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+import blogReducer from "./reducers/blogReducer";
+import notificationReducer from "./reducers/notificationReducer";
+
+const store = configureStore({
+    reducer: {
+        blog: blogReducer, 
+        notification: notificationReducer
+    }
+  })
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+
+);
---
+++ src/reducers/blogReducer.js
@@ -0,0 +1,53 @@
+import { createSlice } from "@reduxjs/toolkit";
+import blogService from "../services/blogs";
+
+const initialState = []
+
+const blogSlice = createSlice({
+    name: "blog",
+    initialState,
+    reducers: {
+        setBlogs(action) {
+            return action.payload
+        },
+        createBlog(state, action) {
+            state.push(action.payload)
+        },
+        updateBlog(state, action) {
+            const newBlog = action.payload
+
+            return state.map(blog => 
+                blog.id === newBlog.id ? newBlog : blog
+            )
+        }
+    }
+})
+
+export const initializeBlog = () => {
+    return async dispatch => {
+        const allBlogs = await blogService.getAll()
+        dispatch(setBlogs(allBlogs))
+    }
+}
+
+export const createNewBlog = (newBlog) => {
+    return async dispatch => {
+        const newBlogObject = await blogService.create(newBlog)
+        dispatch(createBlog(newBlogObject))
+    }
+}
+
+export const increaseBlogLike = (blog) => {
+    return async dispatch => {
+        let newBlogObject = {
+            ...blog,
+            likes: blog.likes + 1,
+        }
+
+        newBlogObject = await blogService.update(newBlogObject.id, newBlogObject)
+        dispatch(updateBlog(newBlogObject))
+    }
+}
+
+export const { setBlogs, createBlog, updateBlog } = blogSlice.actions
+export default blogSlice.reducer
